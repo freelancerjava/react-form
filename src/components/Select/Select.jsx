d15bcd3388ffd346b2fbe201ab22ee9f6ed2a84a
shakxzod_namazbaev <shakxzod.namazbaev.9700@g.ail.com>
2022-02-11 23:26:04 +0500

    push

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,13 +1,13 @@
-import { Select, Option, Wrap, SortIcon } from './style'
+import { Select, Wrap, SortIcon } from './style'
 export default ({ ...props }) => {
     return (
         <Wrap>
             <Select width="230px" height="32px" {...props}>
                 <option value="" hidden>Type</option>
-                <option value="1">Audi</option>
-                <option value="2">BMW</option>
-                <option value="3">Citroen</option>
-                <option value="4">Ford</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+                <option value="4">Option 4</option>
             </Select>
             <SortIcon />
         </Wrap>
